9471240f2aa6aeb43a78a12ad87777d16c0a4453
jdkhusuma <juan.d.khusuma@gmail.com>
2022-05-29 20:55:41 +0700

    Removed /build

--- build/lib/error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handle = void 0;
-function handle(fn) {
-    return fn()
-        .then((data) => [data, null])
-        .catch((error) => [null, error]);
-}
-exports.handle = handle;
