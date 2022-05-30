f1cbc580513044f8c49524a21c5108b03e6ab357
jdkhusuma <juan.d.khusuma@gmail.com>
2022-05-30 23:34:31 +0700

    Access-Control-Allow-Credentials

--- main.ts
+++ main.ts
@@ -30,6 +30,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
+  res.header("Access-Control-Allow-Credentials", "true");
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
     return res.status(200).json({});
