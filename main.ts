660c99d93911825f32dd37d140227dee435697e9
jdkhusuma <juan.d.khusuma@gmail.com>
2022-05-30 23:38:53 +0700

    Access-Control-Allow-Credentials

--- main.ts
+++ main.ts
@@ -33,6 +33,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Credentials", "true");
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    res.header("Access-Control-Allow-Credentials", "true");
     return res.status(200).json({});
   }
   return next();
