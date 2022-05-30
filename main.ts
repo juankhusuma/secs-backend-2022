a1747ec78eb446a5a4b7e3ff07c010bb9f039d3c
jdkhusuma <juan.d.khusuma@gmail.com>
2022-05-30 23:13:43 +0700

    Attempt to resolve CORS error [0]

--- main.ts
+++ main.ts
@@ -24,6 +24,18 @@ const port = process.env.PORT || 5500;
 // Middlewares
 app.use(morgan("dev"));
 app.use(bp.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  return next();
+});
 
 var reqCount = 0;
 
